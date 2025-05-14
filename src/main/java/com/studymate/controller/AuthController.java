d6b9f16c495c6e7964e91600f40f4c625ee889e1
sjnltt3756 <sjnltt3756@gmail.com>
2025-05-14 21:04:10 +0900

    이름 변경

--- src/main/java/com/studymate/controller/AuthController.java
+++ src/main/java/com/studymate/controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
-public class UserController {
+public class AuthController {
 
     private final UserService userService;
 
